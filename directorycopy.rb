456ceb43e3699758eef878f4227ab3be7d7783aa
camilla <cgreene553@gmail.com>
2022-01-21 13:04:22 +0000

    Refactored code except for input_students

--- directorycopy.rb
+++ directorycopy.rb
@@ -1,32 +1,36 @@
-def pluralize(plural)
-    if plural == 1 then "#{plural} student"
-    else "#{plural} students"
-    end
-end
+@students = []
 
 def interactive_menu
-    students = []
-    loop do
-        puts "1. Input the students"
-        puts "2. Show the students"
-        puts "9. Exit"
-        selection = gets.chomp
-        case selection
-        when "1"
-            students = input_students
-        when "2"
-            print_header
-            print(students)
-            print_footer(students)
-        when "9"
-            exit #this will cause  the program to terminate
-        else
-            puts "I don't know what you meant, try again"
-        end
-    end
+  loop do
+    print_menu
+    process(gets.chomp)
+   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
 
 def input_students
     puts "Please enter the names of the students"
@@ -51,40 +55,45 @@ def input_students
             if month == ""
                 month = "July"
             end
-            students << {name: name, cohort: month}
+            @students << {name: name, cohort: month}
           elsif check == "N"
           input_students
           end
           #add the name and cohort month to the array
           #Check if month value is empty and give a default month
           #get another name from the user
-          puts "Now we have #{pluralize students.count}"
+          puts "Now we have #{pluralize @students.count}"
           #get another name from the user
           name = gets.chomp
         end
         students
     end
     
-
+def pluralize(plural)
+    if plural == 1 then "#{plural} student"
+    else "#{plural} students"
+    end
+end
 
 def print_header
     puts "The students of Villains Academy".center(100, ' ')
     puts "-------------".center(100)
 end
 
-def print(students)
-    students.each do |student|
+def print_students_list
+    @students.each do |student|
       student_list = "#{student [:name]}, (#{student[:cohort]} cohort)".split('_').join(' ')
       puts student_list.center(100)
 end
 
-def print_footer(students)
+def print_footer
     puts "-------------".center(100)
-    puts "Overall, we have #{pluralize students.count}".center(100, ' ')
+    puts "Overall, we have #{pluralize @students.count}".center(100, ' ')
 end
 end
+
 interactive_menu
 students = input_students
 print_header
-print(students)
-print_footer(students)
+print_students_list
+print_footer
