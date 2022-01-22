edff8576e8bc3ec3db95c586732b8f859a870ebc
camilla <cgreene553@gmail.com>
2022-01-22 17:40:16 +0000

    Created file that reads its own source code

---
+++ source_code.rb
@@ -0,0 +1,20 @@
+lines = [
+  "lines = [",
+  "__PRINT_OWN_SOURCE_CODE__",
+  "]",
+  "",
+  "lines.each do |line|",
+   " if line == '__PRINT_OWN_SOURCE_CODE__'",
+   "   lines.each { |l| puts ' ' << l.inspect << ',' }",
+   " else",
+   "    puts line",
+   "  end",
+   "end",
+]
+lines.each do |line|
+  if line == '__PRINT_OWN_SOURCE_CODE__'
+    lines.each { |l| puts ' ' << l.inspect << ',' }
+  else
+    puts line
+  end
+end
